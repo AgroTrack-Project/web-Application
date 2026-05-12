e6fb4ff4ec226db695d822f461cc1a40e167f9bf
Eduardo <edudavid2005@hotmail.com>
2026-05-12 06:55:12 -0500

    feat: configure layout with navbar, sidebar and base routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
+import { Layout } from './shared/presentation/components/layout/layout';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', children: [] },
-  { path: 'parcelas', children: [] },
-  { path: 'alertas', children: [] },
-  { path: 'configuracion', children: [] },
-  { path: 'perfil', children: [] },
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: 'home', children: [] },
+      { path: 'parcelas', children: [] },
+      { path: 'alertas', children: [] },
+      { path: 'configuracion', children: [] },
+      { path: 'perfil', children: [] },
+    ]
+  }
 ];
--- src/app/shared/presentation/components/layout/layout.ts
+++ src/app/shared/presentation/components/layout/layout.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {NavbarComponent} from '../navbar/navbar';
+import {RouterOutlet} from '@angular/router';
+import {SidebarComponent} from '../sidebar/sidebar';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [
+    NavbarComponent,
+    RouterOutlet,
+    SidebarComponent
+  ],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
