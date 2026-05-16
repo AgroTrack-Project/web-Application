b0aece61ea99243efc5b69b16d5f677a6f91f3d2
Eduardo <edudavid2005@hotmail.com>
2026-05-16 04:31:25 -0500

    feat: add content on shared

--- src/app/shared/presentation/views/home/home.ts
+++ src/app/shared/presentation/views/home/home.ts
@@ -1,8 +1,11 @@
-import { Component, computed, inject, OnInit } from '@angular/core';
+import { Component, computed, effect, inject, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 import { FarmingStore } from '../../../../farming/application/farming.store';
 import { IdentityStore } from '../../../../identity/application/identity.store';
+import { AlertsStore } from '../../../../alerts/application/alerts.store';
+import { PlotStatus } from '../../../../farming/domain/model/plot-status.enum';
+import { DEPARTMENT_TO_CITY } from '../../../../alerts/domain/constants/peru-department-city';
 
 @Component({
   selector: 'app-home',
@@ -11,13 +14,31 @@ import { IdentityStore } from '../../../../identity/application/identity.store';
   styleUrl: './home.css',
 })
 export class Home implements OnInit {
-  private farmingStore = inject(FarmingStore);
+  private farmingStore  = inject(FarmingStore);
   private identityStore = inject(IdentityStore);
-  private router = inject(Router);
+  private alertsStore   = inject(AlertsStore);
+  private router        = inject(Router);
 
   readonly username = computed(() => this.identityStore.currentUser()?.getName() ?? '');
-  readonly plots = computed(() => this.farmingStore.userPlots());
-  readonly plotCount = computed(() => this.farmingStore.userPlots().length);
+
+  readonly plots = computed(() => {
+    const userId = this.identityStore.currentUserId();
+    return this.farmingStore.plots().filter(
+      p => p.getUserId() === userId && p.getStatus() !== PlotStatus.DELETED
+    );
+  });
+
+  readonly plotCount    = computed(() => this.plots().length);
+  readonly criticalCount = computed(() => this.alertsStore.alerts().length);
+
+  constructor() {
+    effect(() => {
+      const cities = [...new Set(
+        this.plots().map(p => DEPARTMENT_TO_CITY[p.getLocation()] ?? p.getLocation())
+      )];
+      this.alertsStore.loadWeatherForCities(cities);
+    });
+  }
 
   ngOnInit(): void {
     this.farmingStore.loadPlots();
