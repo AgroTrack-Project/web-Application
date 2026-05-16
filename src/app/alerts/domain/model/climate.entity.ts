8c951ce610a9d646f997331a0049563668457300
Miler <mileralexander2003@gmail.com>
2026-05-16 02:55:27 -0500

    feat: add climate entity class to represent climate data

---
+++ src/app/alerts/domain/model/climate.entity.ts
@@ -0,0 +1,15 @@
+export class ClimateEntity {
+
+  constructor(
+    public city: string,
+    public country: string,
+    public temperature: number,
+    public humidity: number,
+    public description: string,
+    public icon: string,
+    public windSpeed: number,
+    public rainRisk: string,
+    public droughtRisk: string,
+    public heatRisk: string,
+  ) {}
+}
