40b16bd1bbf573ae18abdc28690b239f4fe66a11
Miler2003 <mileralexander2003@gmail.com>
2026-07-08 18:23:54 -0500

    feature: add environment configuration file for development

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:3000',
+  usersEndpointPath: '/users',
+  plansEndpointPath: '/plans',
+  alertPreferencesEndpointPath: '/alert-preferences',
+  plotsEndpointPath: '/plots',
+  cropsEndpointPath: '/crops',
+  soilRecordsEndpointPath: '/soil-records',
+  irrigationRecommendationsEndpointPath: '/irrigation-recommendations',
+  openWeatherApiUrl: 'https://api.openweathermap.org/data/2.5',
+  openWeatherApiKey: '',
+};
