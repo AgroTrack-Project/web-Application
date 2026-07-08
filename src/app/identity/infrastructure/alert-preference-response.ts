68f2680759f91c12a29a501aae97c57609cfba04
Miler2003 <mileralexander2003@gmail.com>
2026-07-08 17:56:54 -0500

    feature: add alert preference response model and resource interface

--- src/app/identity/infrastructure/alert-preference-response.ts
+++
@@ -1,9 +0,0 @@
-import { BaseResource } from '../../shared/infrastructure/base-response';
-
-export interface AlertPreferenceResource extends BaseResource {
-  id: string;
-  user_id: string;
-  frost_enabled: boolean;
-  drought_enabled: boolean;
-  heavy_rain_enabled: boolean;
-}
