2fc144f4ef8dac02448982fdb6fefcceb26063a6
Miler2003 <mileralexander2003@gmail.com>
2026-07-08 18:24:57 -0500

    feature: add ProPlan entity to IAM domain model

---
+++ src/app/iam/domain/model/pro-plan.entity.ts
@@ -0,0 +1,15 @@
+import { Plan } from './plan.entity';
+
+export class ProPlan implements Plan {
+  readonly id: string;
+  private readonly price = 85.00;
+  private readonly maxPlots = 10;
+
+  constructor(id = '') { this.id = id; }
+
+  getMaxPlots(): number { return this.maxPlots; }
+  getPrice(): number { return this.price; }
+  isDashboardEnabled(): boolean { return true; }
+  isExportEnabled(): boolean { return true; }
+  hasPrioritySupport(): boolean { return false; }
+}
